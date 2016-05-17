7faa83b3f31bfa65aaa50e8478568f59f9c36832
James Frick <jamesmfrick@gmail.com>
2016-05-17 16:18:00 -0400

    data creation

--- scripts/helper.py
+++ scripts/helper.py
@@ -1,16 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jul 16 19:51:39 2015
+Created on Fri May 6 19:51:39 2016
 
-@author: jmf
+@author: frickjm
 
-Creates documents for each user by:
-
-1) obtaining the mean rating for each movie
-2) writes a term in the user's document "like_movieID" or "dislike_movieID"
-    - if rating > mean, write like
-    - if rating < mean, write dislike
-    - write term proportional to stdev above/below mean 
+utility functions for RNN recommender systems
 """
 
 import sys
@@ -19,87 +13,134 @@ from os.path import isfile
 import numpy as np
 import cPickle
 
-"""iterate over unsplit ratings file to get vocabularies for each embedder"""
+
 def getVocabularies(ratingsFile):
-    movieIDs    = {}
+    """
+    iterate over unsplit ratings file to get vocabularies for each embedder
+    
+    Returns:
+        itemIds:
+            dictionary of {'item_ID':'index'}
+            
+        userIDs:
+            dictionary of {'user_ID':'index'}
+    """
+    itemIDs    = {}
     userIDs     = {}
 
     with open(ratingsFile,'rb') as f:
         for rating in f:
             tsp = rating.split("::")
             if len(tsp) > 1:
-                movie   = str(int(tsp[1]))
-                user    = tsp[0]
+                item   = int(tsp[1])
+                user    = int(tsp[0])
                 if user not in userIDs:
                     userIDs[user] = len(userIDs)
-                if movie not in movieIDs:
-                    movieIDs[movie] = len(movieIDs)
-    return movieIDs, userIDs
+                if item not in itemIDs:
+                    itemIDs[item] = len(itemIDs)
+    return itemIDs, userIDs
 
-"""takes in a line from ratings and returns what to write in that user's doc"""    
-def writeDoc(fields,ratings,STD_SCALAR):
-    movie           = str(int(fields[1]))
-    user_rating     = float(fields[2])
-    value           = ratings[movie]
-    mean_rating     = float(value[0])
-    std_rating      = float(value[1])+.00000001
-    z               = (user_rating-mean_rating)/std_rating
-    writeNum        = z*STD_SCALAR
 
-    #This returns the word to write and how many times to write it in a tuple
-    if z > 0:
-        out = ("L_"+movie+" ",int(np.floor(writeNum)))
-    elif z < 0:
-        out = ("D_"+movie+" ",int(np.floor(-writeNum)))
-    else:
-        out = ("",0)
-    return out
 
-"""
-gets the mean and stdev rating for each movie
-returns a dict of "movie": "[mean,stdev]" 
-"""
 def ratingsAvg(trainFile):
-    movies  = {}
+    """
+    gets the mean and stdev rating for each item
+    returns a dict of "item": "[mean,stdev]" 
+    """
+    items  = {}
     #dsp = data.split("\n")
     with open(trainFile,'rb') as f:
         for rating in f:
             tsp = rating.split("::")
             if len(tsp) > 1:
-                if movies.has_key(tsp[1]):
-                    movies[tsp[1]].append(float(tsp[2]))
+                if items.has_key(int(tsp[1])):
+                    items[int(tsp[1])].append(float(tsp[2]))
                 else:
-                    movies[tsp[1]]  = [float(tsp[2])]
+                    items[int(tsp[1])]  = [float(tsp[2])]
         out ={}            
-        for k,v in movies.iteritems():
-            out[k]  = str(np.mean(v))+"\t"+str(np.std(v))
+        for k,v in items.iteritems():
+            out[k]  = [np.mean(v), np.std(v)]
         
     return out
+  
 
-"""gets the number of ratings given and the average rating for each user"""
 def userRatings(trainFile):
-    with open(trainFile,'rb') as f1:
+    """
+    Returns two dicts indexed by user_id
+    
+    userRatings:
+        contains all a user's ratings in the form [item_ID, rating]
         
-        userStats   = {}
+    userStats:
+        contains a user's aggregate info (based only on training set)
+        [user_rating_mean, user_rating_std, user_num_ratings]
+        
+    Note: 
+        more information can easily be added to userStats to increase the "content 
+        based" aspect of this approach
+    """
+    
+    with open(trainFile,'rb') as f1:
+        userRatings   = {}
         for rating in f1:
             tsp = rating.split("::")
             if len(tsp) > 1:
-                if userStats.has_key(tsp[0]):
-                    userStats[tsp[0]].append(float(tsp[2]))
+                if userRatings.has_key(int(tsp[0])):
+                    userRatings[int(tsp[0])].append(np.array([int(tsp[1]),float(tsp[2])]))
                 else:
-                    userStats[tsp[0]]  = [float(tsp[2])]
-        out = {}        
+                    userRatings[int(tsp[0])]  = [np.array([int(tsp[1]),float(tsp[2])])]
+        userStats = {}        
         
         #calculate the number of ratings each user has in training set, then scale
         ratTots     = []
-        for k,v in userStats.iteritems():
+        for k,v in userRatings.iteritems():
             ratTots.append(len(v))
         numMean     = np.mean(np.array(ratTots))
         numSTD      = np.std(np.array(ratTots))
 
         #return dict by userID with mean rating and z-scores on number of ratings given
-        for k,v in userStats.iteritems():
-            norm    = (len(v) - numMean)/numSTD
-            out[k]  = str(np.mean(v))+"\t"+str(norm)
-    return out
+        for k,v in userRatings.iteritems():
+            norm    = (len(v) - numMean)/numSTD #number of items rated (z-score)
+            rats = [r[1] for r in v]
+            rmean = np.mean(rats) #mean rating for user
+            rstd = np.std(rats) #std of user's ratings
+            userStats[k]  = np.array([rmean,rstd, norm])
+    return userRatings, userStats
     
+
+def getSample(toSample,numToPick=11):
+    if type(toSample) != np.ndarray:
+        toSample = np.array(toSample)
+    inds = np.random.choice(toSample.shape[0],numToPick,replace=False)
+    ts = toSample[inds]
+    past = ts[:-1]
+    current = ts[-1]
+    return past, current
+    
+def createExample(past,current,userStats,itemStats,itemIDs,userID,predict=False):
+    pastItemInds     = [itemIDs[int(i[0])] for i in past]
+    pastRatings = [float(i[1]) for i in past]
+    print(pastItemInds)
+    currentItemInd = itemIDs[int(current[0])]*len(pastItemInds)
+    print(currentItemInd)
+    if not predict:
+        currentRating = itemIDs[int(current[1])]
+    else:
+        currentRating = None
+    
+    #building the aggregate information vector - starting with item information
+    aggregates = [np.array(itemStats[int(i[0])]) for i in past]
+
+    #add user information to the vector
+    userInfo = list(userStats[userID])
+    aggregates = [np.append(agg,userInfo,axis=0) for agg in aggregates]    
+
+    #add current item information to the vector
+    itemInfo = list(itemStats[int(current[0])])
+    aggregates = [np.append(agg,itemInfo,axis=0) for agg in aggregates] 
+    
+    #add past rating information for each item to aggregate vector
+    aggregates = [np.append(agg,pastRatings[i]) for i,agg in enumerate(aggregates)] 
+    print(aggregates)
+    
+    return pastItemInds, currentItemInd, aggregates, currentRating
--- scripts/wholeRun.py
+++ scripts/wholeRun.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri May  6 12:52:21 2016
+Created on Fri May 6 12:52:21 2016
 
 Runs through k-folds to evaluate recommender system performance using RMSE on the MovieLens dataset
 
@@ -16,12 +16,18 @@ import helper
 #subprocess.call("../data/raw/split_ratings.sh")
 
 num_folds   = 5
+num_folds = 1
 
 print(listdir("../data/raw"))
-movieIDs, userIDs   = helper.getVocabularies("../data/raw/ratings.dat")
+itemIDs, userIDs   = helper.getVocabularies("../data/raw/ratings.dat")
 for fold in range(0,num_folds):
-    userRatings     = helper.userRatings("../data/raw/r"+str(fold+1)+".train")
-    movieRatings    = helper.ratingsAvg("../data/raw/r"+str(fold+1)+".train")
-
-
+    userRatings,userStats = helper.userRatings("../data/raw/r"+str(fold+1)+".train")
+    itemStats    = helper.ratingsAvg("../data/raw/r"+str(fold+1)+".train")
+    
+    for person in userRatings.keys():
+        print(helper.getSample(userRatings[person]))
+        past, current = helper.getSample(userRatings[person])
+        pastItemInds, currentItemInd, aggregates, currentRating = helper.createExample(past,current,userStats,itemStats,itemIDs,person)
+        stop=raw_input()
+    
 
