e044b952bae25a35ab9e692265bb71e2baf7e8fa
James Frick <jamesmfrick@gmail.com>
2016-05-18 16:03:15 -0400

    update

--- scripts/helper.py
+++ scripts/helper.py
@@ -109,6 +109,19 @@ def userRatings(trainFile):
     
 
 def getSample(toSample,numToPick=11):
+    """
+    samples from the input. If not an array, calls np.array() on input first.
+    
+    in this context, used to pick ratings from a user
+    
+    input:
+        toSample:
+            list or np.array object from which you want to sample
+        numToPick:
+            integer determining how many items to sample
+
+    """        
+        
     if type(toSample) != np.ndarray:
         toSample = np.array(toSample)
     inds = np.random.choice(toSample.shape[0],numToPick,replace=False)
@@ -118,10 +131,41 @@ def getSample(toSample,numToPick=11):
     return past, current
     
 def createExample(past,current,userStats,itemStats,itemIDs,userID,predict=False):
+    """
+    takes a sample of user ratings and an item to evaluate and returns matrices
+    for the input as well as the output rating
+    
+    input:
+        past:
+            user's past ratings in the form [[item_ID, rating],[],...]
+        current:
+            rating to be predicted in the form [item_ID,rating] (rating=None if predict=True)
+        userStats:
+            characteristics of a user vectorized (generally summary stats about ratings)
+        itemStats:
+            characteristics of an item vectorized (generally summary stats)
+        itemIDs:
+            dictionary mapping item_ID to index in the embedder's vocabulary
+        userID:
+            ID of the current user
+        predict:
+            Boolean determining if training or predicting
+            
+    output:
+        pastItemInds:
+            np.array of shape (seqLength,) containing indices of item_IDs for embedder
+        currentItemInd:
+            np.array of shape (seqLength,) containing the index of the item_ID being predicted
+        aggregates:
+            np.array containing all other information available at each time step (seqLength,aggSize)
+        currentRating:
+            rating of current item by current user (used for training or scoring)
+    """
+    
     pastItemInds     = [itemIDs[int(i[0])] for i in past]
     pastRatings = [float(i[1]) for i in past]
     print(pastItemInds)
-    currentItemInd = itemIDs[int(current[0])]*len(pastItemInds)
+    currentItemInd = np.array([itemIDs[int(current[0])] for i in range(0,len(pastItemInds))])
     print(currentItemInd)
     if not predict:
         currentRating = itemIDs[int(current[1])]
@@ -141,6 +185,38 @@ def createExample(past,current,userStats,itemStats,itemIDs,userID,predict=False)
     
     #add past rating information for each item to aggregate vector
     aggregates = [np.append(agg,pastRatings[i]) for i,agg in enumerate(aggregates)] 
+    aggregates = np.reshape(aggregates,(len(aggregates),len(aggregates[0])))
     print(aggregates)
     
-    return pastItemInds, currentItemInd, aggregates, currentRating
+    return np.array(pastItemInds), currentItemInd, aggregates, currentRating
+    
+
+def getEpoch(ratings,userStats,itemStats,itemIDs,userIDs,predict=False):
+    numExamples = len(ratings)
+    
+    #get one example to determine sizes
+    past, current = getSample(userRatings[userIDs.keys()[0]])
+    pastItemInds, currentItemInd, aggregates, currentRating = createExample(past,current,userStats,itemStats,itemIDs,userIDs.keys()[0])    
+    
+    
+    aggregateX = np.zeros(numExamples,aggregates.shape[0],aggregates.shape[1])
+    pastItemX = np.zeros(numExamples,pastItemInds.shape[0])
+    currentItemX = np.zeros(numExamples,currentItemInd.shape[0])
+    y = np.zeros(numExamples)
+    
+    for num,line in enumerate(ratings):
+        spl     = line.split("::")
+        if len(tsp) > 1:
+            past, current = getSample(userRatings[int(tsp[0])])
+            pastItemInds, currentItemInd, aggregates, currentRating = createExample(past,current,userStats,itemStats,itemIDs,int(tsp[0])) 
+            
+            aggregateX[num] = aggregates
+            pastItemX[num] = pastItemInds
+            currentItemX[num] = currentItemInd
+            y[num] = currentRating
+        
+        if num % 10000 ==0:
+            print(num)
+            
+    return aggregateX, pastItemX, currentItemX, y
+            
--- scripts/wholeRun.py
+++ scripts/wholeRun.py
@@ -19,15 +19,23 @@ num_folds   = 5
 num_folds = 1
 
 print(listdir("../data/raw"))
+
+with open("../data/raw/ratings.dat",'rb') as f:
+    ratings = [line for line in f.read().split("\n") if line != '']
+
 itemIDs, userIDs   = helper.getVocabularies("../data/raw/ratings.dat")
 for fold in range(0,num_folds):
     userRatings,userStats = helper.userRatings("../data/raw/r"+str(fold+1)+".train")
     itemStats    = helper.ratingsAvg("../data/raw/r"+str(fold+1)+".train")
     
-    for person in userRatings.keys():
-        print(helper.getSample(userRatings[person]))
-        past, current = helper.getSample(userRatings[person])
-        pastItemInds, currentItemInd, aggregates, currentRating = helper.createExample(past,current,userStats,itemStats,itemIDs,person)
-        stop=raw_input()
+    aggregateX, pastItemX, currentItemX, y = helper.getEpoch(ratings,userStats,itemStats,itemIDs,userIDs)
+#    for person in userRatings.keys():
+#        print(helper.getSample(userRatings[person]))
+#        past, current = helper.getSample(userRatings[person])
+#        pastItemInds, currentItemInd, aggregates, currentRating = helper.createExample(past,current,userStats,itemStats,itemIDs,person)
+#        print(pastItemInds.shape)
+#        print(currentItemInd.shape)
+#        print(aggregates.shape)
+#        stop=raw_input()
     
 
